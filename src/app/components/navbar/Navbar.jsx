8b30a3739bd3041d373c16aad8f928499ba04211
sagar jhode <sagar.jhode@iviewlabs.net>
2022-09-29 17:24:41 +0530

    changed path for netlify server

--- src/app/components/navbar/Navbar.jsx
+++ src/app/components/navbar/Navbar.jsx
@@ -37,9 +37,9 @@ const Navbar = () => {
             </h2>
           </div>
           <div className="flex">
-            {/* {window.location.href === 
-          "https://my-restaurant1.netlify.app/search" ? ( */}
-            {window.location.href === "http://localhost:3000/search" ? (
+            {/* {window.location.href === "http://localhost:3000/search" ? ( */}
+            {window.location.href === 
+          "https://my-restaurant1.netlify.app/search" ? (
               <Link to="/settings">
                 <button className="bg-white text-black font-bold h-9 w-24 mr-9 rounded-md hover:bg-green-500 hover:text-white">
                   Settings
